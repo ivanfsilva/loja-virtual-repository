d87fc31b187d779a8eb75a3a35fe3c83022be52d
ivanfsilva <ivanfs27@gmail.com>
2020-09-11 15:21:15 -0300

    criacao do DAO

--- src/br/com/ivanfsilva/jdbc/TestaInsercaoComProduto.java
+++ src/br/com/ivanfsilva/jdbc/TestaInsercaoComProduto.java
@@ -1,12 +1,10 @@
 package br.com.ivanfsilva.jdbc;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-import model.Produto;
+import br.com.ivanfsilva.jdbc.dao.ProdutoDAO;
+import br.com.ivanfsilva.jdbc.model.Produto;
 
 public class TestaInsercaoComProduto {
 
@@ -15,22 +13,8 @@ public class TestaInsercaoComProduto {
 		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
 		
 		try(Connection connection = new ConnectionFactory().recuperarConexao()) {
-			String sql = "INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)";
-			
-			try(PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-				pstm.setString(1, comoda.getNome());
-				pstm.setString(2, comoda.getDescricao());
-				
-				pstm.execute();
-				
-				try(ResultSet rst = pstm.getGeneratedKeys()) {
-					while(rst.next()) {
-						comoda.setId(rst.getInt(1));
-					}
-				}
-			}
+			ProdutoDAO produtoDao = new ProdutoDAO(connection);
+			produtoDao.salvar(comoda);
 		}
-
 	}
-
 }
--- src/br/com/ivanfsilva/jdbc/model/Produto.java
+++ src/br/com/ivanfsilva/jdbc/model/Produto.java
@@ -1,4 +1,4 @@
-package model;
+package br.com.ivanfsilva.jdbc.model;
 
 public class Produto {
 
